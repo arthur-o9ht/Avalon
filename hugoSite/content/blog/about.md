9d29cdb39c9c9fe9a14a61bf3339221f32595005
Arthur <renlong@didichuxing.com>
2018-08-19 23:47:33 +0800

    修改本地port

--- hugoSite/content/blog/about.md
+++
@@ -1,7 +0,0 @@
----
-title: "About"
-date: 2018-08-05T00:56:15+08:00
-draft: true
----
-
-
--- src/main.go
+++ src/main.go
@@ -2,11 +2,24 @@ package main
 
 import (
 	"github.com/go-martini/martini"
-	)
+	"os"
+	"fmt"
+)
+
+var port string
+
+func init(){
+	name,_ := os.Hostname()
+	if name == "ArthurdeMacBook-Pro.local" {
+		port = ":8080"
+	}else{
+		port = ":80"
+	}
+	fmt.Println(name)
+}
 
 func main() {
 	m := martini.Classic()
 	m.Use(martini.Static("hugoSite/public"))
-	m.RunOnAddr(":80")
-	//m.RunOnAddr(":8080")
+	m.RunOnAddr(port)
 }
